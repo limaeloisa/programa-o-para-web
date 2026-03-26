fc529d8a380b5122a1fb7b3533bda0a7c73f84a1
eloisa <144168277+limaeloisa@users.noreply.github.com>
2026-03-26 16:14:45 -0300

    update

--- letras-alfabeto/script.js
+++ letras-alfabeto/script.js
@@ -1,8 +1,7 @@
 const alfabeto = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
-  'U', 'V', 'W', 'X', 'Y', 'Z'                      
-];
+  'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 document.write(
   alfabeto[9] + 
